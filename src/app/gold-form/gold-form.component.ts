a22fbe244d8ac69390a24dd3348b8e8882051321
Praveen Kumar <bpk@Praveens-MacBook-Pro.local>
2020-08-01 18:29:16 +0530

    002-After-2-Module

--- src/app/gold-form/gold-form.component.ts
+++ src/app/gold-form/gold-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PriceListService } from '../services/price-list.servie';
+import { IYearPrice, IRangePercentage } from '../model/year-price.model';
+import { SelectItem } from 'primeng/api';
 @Component({
   selector: 'app-gold-form',
   templateUrl: './gold-form.component.html',
@@ -7,9 +9,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GoldFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(private priceListService:PriceListService) { }
+
+  public index: number = -1;
+  public selectedYearRange: SelectItem = Object.assign({});
 
   ngOnInit(): void {
+    this.priceListService.init();
+    this.selectedYearRange = this.yearDifferenceDropdown[0];
   }
+
+  get priceList() : Array<IYearPrice>{
+    return this.priceListService.priceListing;
+  }
+
+  get percentageList() : Array<IRangePercentage>{
+    return this.priceListService.yearPercentage;
+  }
+
+  get averagePercentageIncreased(): number{
+    if(this.priceListService.averagePercentageIncreased){
+      return this.priceListService.averagePercentageIncreased;
+    }
+    return 0;
+  }
+  get yearDifferenceDropdown() : Array<SelectItem>{
+    return this.priceListService.yearDifferenceDropdown;
+  }
+  public openPrev(){
+    this.index = (this.index <= 0) ? 3 : this.index - 1;
+  }
+
+
+  public openNext(){
+    this.index = (this.index === 3) ? 0 : this.index + 1;
+  }
+
+  public onValueChange(){
+    this.priceListService.rangeDropdownChanged(this.selectedYearRange);
+  }
+
+
+  public getGramValue(price: number) : number{
+    if(price){
+      return price/10;
+    }
+    return 0;
+  }
+
+  public getPoundValue(price: number) : number{
+    if(price){
+      return (price/10)*8;
+    }
+    return 0;
+  }
+
+  // public openPrev(){
+    
+  // }
 
 }
---
+++ src/app/model/year-price.model.ts
@@ -0,0 +1,8 @@
+export interface IYearPrice{
+    year: number;
+    price: number;
+}
+export interface IRangePercentage{
+    yearRange: string;
+    percentageChanged: number;
+}
