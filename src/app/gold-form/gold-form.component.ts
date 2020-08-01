a826d83bbb6a9d6c0eb8f77c3196123df33ebd2f
Praveen Kumar <bpk@Praveens-MacBook-Pro.local>
2020-08-01 13:10:24 +0530

    001-Create App

---
+++ src/app/gold-form/gold-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gold-form',
+  templateUrl: './gold-form.component.html',
+  styleUrls: ['./gold-form.component.scss']
+})
+export class GoldFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
